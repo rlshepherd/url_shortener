1da29ad313a1c56ae37999378845eccf1476fe35
Russell Shepherd <russleeshepherd@gmail.com>
2019-01-03 14:40:26 +0800

    add basic routes to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 require 'sinatra'
 require 'sinatra/activerecord'
+require './config/environment.rb'
+
+get '/' do
+end
+
+post '/' do
+end
+
+get '/:shortened' do
+  shortened_url = ShortenedUrl.find(params[:shortened])
+  redirect shortened_url.url
+end
